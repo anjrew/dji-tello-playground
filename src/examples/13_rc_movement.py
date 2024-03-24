253bcc585f9fa9d019d375874b95234d24649ac2
Andrew Johnson <earyzhe@gmail.com>
2024-03-24 11:23:41 +0100

    Add rc movement script

--- src/examples/13_rc_movement.py
+++ src/examples/13_rc_movement.py
@@ -4,6 +4,7 @@ from djitellopy import Tello
 import time
 
 SPEED_SETTING_CM_S = 30
+MOVEMENT_MAGNITUDE = 10
 
 # Create a Tello instance
 tello = Tello()
@@ -24,7 +25,8 @@ print("Setting speed to", SPEED_SETTING_CM_S)
 tello.set_speed(SPEED_SETTING_CM_S)
 
 
-def do_action_for_time(action: Callable, time_in_seconds: int):
+def do_action_for_time(label: str, action: Callable, time_in_seconds: int):
+    print(label)
     action()
     time.sleep(time_in_seconds)
 
@@ -34,6 +36,7 @@ forward_backward_velocity = 0
 up_down_velocity = 0
 yaw_velocity = 0
 do_action_for_time(
+    "Staying still",
     lambda: tello.send_rc_control(
         left_right_velocity, forward_backward_velocity, up_down_velocity, yaw_velocity
     ),
@@ -42,10 +45,11 @@ do_action_for_time(
 
 # Move forward
 left_right_velocity = 0
-forward_backward_velocity = 1
+forward_backward_velocity = MOVEMENT_MAGNITUDE
 up_down_velocity = 0
 yaw_velocity = 0
 do_action_for_time(
+    "Moving Forward",
     lambda: tello.send_rc_control(
         left_right_velocity, forward_backward_velocity, up_down_velocity, yaw_velocity
     ),
@@ -54,10 +58,11 @@ do_action_for_time(
 
 # Move Backwards
 left_right_velocity = 0
-forward_backward_velocity = -1
+forward_backward_velocity = -MOVEMENT_MAGNITUDE
 up_down_velocity = 0
 yaw_velocity = 0
 do_action_for_time(
+    "Moving backwards",
     lambda: tello.send_rc_control(
         left_right_velocity, forward_backward_velocity, up_down_velocity, yaw_velocity
     ),
@@ -66,11 +71,12 @@ do_action_for_time(
 
 
 # Move Left
-left_right_velocity = 1
+left_right_velocity = MOVEMENT_MAGNITUDE
 forward_backward_velocity = 0
 up_down_velocity = 0
 yaw_velocity = 0
 do_action_for_time(
+    "Moving left",
     lambda: tello.send_rc_control(
         left_right_velocity, forward_backward_velocity, up_down_velocity, yaw_velocity
     ),
@@ -78,11 +84,12 @@ do_action_for_time(
 )
 
 # Move Right
-left_right_velocity = -1
+left_right_velocity = -MOVEMENT_MAGNITUDE
 forward_backward_velocity = 0
 up_down_velocity = 0
 yaw_velocity = 0
 do_action_for_time(
+    "Moving right",
     lambda: tello.send_rc_control(
         left_right_velocity, forward_backward_velocity, up_down_velocity, yaw_velocity
     ),
@@ -92,9 +99,10 @@ do_action_for_time(
 # Move Up
 left_right_velocity = 0
 forward_backward_velocity = 0
-up_down_velocity = 1
+up_down_velocity = MOVEMENT_MAGNITUDE
 yaw_velocity = 0
 do_action_for_time(
+    "Moving up",
     lambda: tello.send_rc_control(
         left_right_velocity, forward_backward_velocity, up_down_velocity, yaw_velocity
     ),
@@ -104,9 +112,10 @@ do_action_for_time(
 # Move down
 left_right_velocity = 0
 forward_backward_velocity = 0
-up_down_velocity = -1
+up_down_velocity = -MOVEMENT_MAGNITUDE
 yaw_velocity = 0
 do_action_for_time(
+    "Moving down",
     lambda: tello.send_rc_control(
         left_right_velocity, forward_backward_velocity, up_down_velocity, yaw_velocity
     ),
@@ -117,8 +126,9 @@ do_action_for_time(
 left_right_velocity = 0
 forward_backward_velocity = 0
 up_down_velocity = 0
-yaw_velocity = 1
+yaw_velocity = MOVEMENT_MAGNITUDE
 do_action_for_time(
+    "Turning left",
     lambda: tello.send_rc_control(
         left_right_velocity, forward_backward_velocity, up_down_velocity, yaw_velocity
     ),
@@ -129,8 +139,9 @@ do_action_for_time(
 left_right_velocity = 0
 forward_backward_velocity = 0
 up_down_velocity = 0
-yaw_velocity = -1
+yaw_velocity = -MOVEMENT_MAGNITUDE
 do_action_for_time(
+    "Turning right",
     lambda: tello.send_rc_control(
         left_right_velocity, forward_backward_velocity, up_down_velocity, yaw_velocity
     ),
