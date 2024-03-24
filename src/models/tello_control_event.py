e97fc450e8383184bbfaad10594cccc4a1e7d0e2
Andrew Johnson <andrewmjohnson549@gmail.com>
2024-03-24 18:29:23 +0100

    Remove unused files

--- src/models/tello_control_event.py
+++
@@ -1,7 +0,0 @@
-from enums.tello_action_type import TelloActionType
-
-
-class TelloControlEvent:
-    def __init__(self, action: TelloActionType, intensity: float):
-        self.action = action
-        self.intensity = intensity
