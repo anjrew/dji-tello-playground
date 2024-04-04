3b4fcc7682a58f28a25932d633d349d4a374c027
Andrew Johnson <earyzhe@gmail.com>
2024-04-04 13:17:42 +0100

    Finish auto detect controller

--- src/auto_detect_controller.py
+++ src/auto_detect_controller.py
@@ -5,7 +5,7 @@ script_dir = os.path.dirname(__file__)
 parent_dir = os.path.join(script_dir, "..")
 sys.path.append(parent_dir)
 
-from typing import Dict, Callable
+from typing import Dict, Callable, Optional
 from services.tello_command_dispatcher import TelloCommandDispatcher
 from services.tello_connector import TelloConnector
 from djitellopy import Tello
@@ -57,20 +57,23 @@ def main(
     logging.basicConfig(level=log_level)
     LOGGER = logging.getLogger(__name__)
 
-    pygame_connector = PyGameConnector()
-    pygame_connector.init_joystick()
-    joystick = pygame_connector.create_joystick(0)
-    joystick.init()
-
-    name = joystick.get_name()
-    platform = sys.platform
-
+    controller: Optional[TelloController] = None
     try:
+
+        pygame_connector = PyGameConnector()
+        pygame_connector.init_joystick()
+        joystick = pygame_connector.create_joystick(0)
+        joystick.init()
+
+        name = joystick.get_name()
+        platform = sys.platform
+
         controller_type = get_controller_type(platform, name)
         controller = controller_mapping[controller_type](pygame_connector)
     except (KeyError, ValueError) as e:
         LOGGER.error(f"Error detecting controller: {e}")
-        return
+        LOGGER.info("Defaulting to Keyboard Controller")
+        controller = KeyboardControlAdapter(PyGameConnector())
 
     tello = Tello()
     tello_service = TelloConnector(tello)
